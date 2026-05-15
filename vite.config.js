624192b9637abd58162e490a490a47e5d66493ef
Drloudx <1965161643@qq.com>
2026-05-16 03:21:40 +0800

    fix vite config import error

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vite-plugin-vue'
+import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/test/',
-
+  base: '/test/', // GitHub Pages 必须加这个
   plugins: [
     vue(),
     vueDevTools(),
