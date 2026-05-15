e171b94b089d36970f56e472dbd28f819d3bd186
Drloudx <162087764+Drloudx@users.noreply.github.com>
2026-05-16 05:01:26 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // 👇 关键配置：强制不打包 wasm 文件
   build: {
     assetsInlineLimit: 0,
     rollupOptions: {
