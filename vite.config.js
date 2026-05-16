f38274e75602ed88d868cb7a5408b30386e309ce
Drloudx <1965161643@qq.com>
2026-05-16 08:58:10 +0800

    vite.config.js更新

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import fs from 'fs'
-import path from 'path'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
+  // ⚡️ 修正：Cloudflare Pages 部署在根目录，必须改为 '/'
   base: '/',
-=======
-  // 关键改动：改为相对路径，适配 Cloudflare Pages 根目录部署
-  base: './', 
->>>>>>> a93ccab741a631f1acfec523d2a941e227c5f5e7
   plugins: [
     vue(),
-    {
-      name: 'cleanup-wasm',
-      closeBundle() {
-        const distAssets = path.resolve(__dirname, 'dist/assets');
-        if (fs.existsSync(distAssets)) {
-          const files = fs.readdirSync(distAssets);
-          files.forEach(file => {
-            if (file.endsWith('.wasm')) {
-              fs.unlinkSync(path.join(distAssets, file));
-              console.log(`已强制删除超大文件: ${file}`);
-            }
-          });
-        }
-      }
-    }
+    vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  }
+  },
 })
